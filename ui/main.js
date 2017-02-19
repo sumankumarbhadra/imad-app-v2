034af597d4effba4199d9d16271e8cf8d3d79a3a
sumankumarbhadra <sumankumarbhadra@gmail.com>
2017-02-19 21:20:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,31 +11,8 @@ button.onclick=function(){
 	span.innerHTML = likecounter.toString();
 };
 
-//create request by id
-/*var request = new XMLHttpRequest();
-
-//capture the request and store in a variable
-request.onreadystatechange = function(){
-if(request.readyState===XMLHttpRequest.DONE){
-//Take some action
-if(request.state===200){
-	var likecounter = request.responceText;
-	var span = document.getElementById('likes');
-	span.innerHTML = likecounter.toString();
-	
-		}
-	}
-	//Not the request
-};
-
-//Make request
-request.open("GET","http://sumankumarbhadra.imad.hasura-ap/likes",true);
-request.send(null);
-};
-//counter code 
  
-*/
-
+/*
 //create request by id
 var request = new XMLHttpRequest();
 
@@ -57,4 +34,4 @@ if(request.state===200){
 request.open('GET','http://sumankumarbhadra.imad.hasura-app.io/',true);
 request.send(null);
 
-
+*/
