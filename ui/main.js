5f234ad11ef3fe50b1f99b967cc640b7ada945df
sumankumarbhadra <sumankumarbhadra@gmail.com>
2017-02-19 19:11:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,56 @@
 console.log('Loaded!');
+//counter code 
+var button = document.getElementById('counter');
+
+button.onclick = function(){
+
+//create request by id
+var request = new XMLHttpRequest();
+
+//capture the request and store in a variable
+request.onreadystatechange = function(){
+if(request.readyState==XMLHttpRequest.DONE){
+//Take some action
+if(request.State==200){
+	var counter = request.responceText;
+	var span = document.getElementById('count');
+	span.innerHTML = counter.toString();
+	
+		}
+	}
+	//Not the request
+};
+
+//Make request
+request.open('GET','',true);
+request.send(null);
+};
+
+// code for likes
+
+//counter code 
+var button = document.getElementById('like');
+
+button.onclick = function(){
+
+//create request by id
+var request = new XMLHttpRequest();
+
+//capture the request and store in a variable
+request.onreadystatechange = function(){
+if(request.readyState==XMLHttpRequest.DONE){
+//Take some action
+if(request.State==200){
+	var counter = request.responceText;
+	var span = document.getElementById('likes');
+	span.innerHTML = counter.toString();
+	
+		}
+	}
+	//Not the request
+};
+
+//Make request
+request.open('GET','',true);
+request.send(null);
+};
