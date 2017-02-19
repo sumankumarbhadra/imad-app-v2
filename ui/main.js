286795c136e402312d4cc5920da40f4dec442854
sumankumarbhadra <sumankumarbhadra@gmail.com>
2017-02-19 19:39:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,31 +1,4 @@
 console.log('Loaded!');
-//counter code 
-var button = document.getElementById('counter');
-
-button.onclick=function(){
-
-//create request by id
-var request = new XMLHttpRequest();
-
-//capture the request and store in a variable
-request.onreadystatechange = function(){
-if(request.readyState==XMLHttpRequest.DONE){
-//Take some action
-if(request.State==200){
-	var counter = request.responceText;
-	var span = document.getElementById('count');
-	span.innerHTML = counter.toString();
-	
-		}
-	}
-	//Not the request
-};
-
-//Make request
-request.open('GET','',true);
-request.send(null);
-};
-
 // code for likes
 
 //counter code 
@@ -54,3 +27,30 @@ if(request.State==200){
 request.open('GET','http://sumankumarbhadra.imad.hasura-app.io/likes',true);
 request.send(null);
 };
+//counter code 
+var button = document.getElementById('counter');
+
+button.onclick=function(){
+
+//create request by id
+var request = new XMLHttpRequest();
+
+//capture the request and store in a variable
+request.onreadystatechange = function(){
+if(request.readyState==XMLHttpRequest.DONE){
+//Take some action
+if(request.State==200){
+	var counter = request.responceText;
+	var span = document.getElementById('count');
+	span.innerHTML = counter.toString();
+	
+		}
+	}
+	//Not the request
+};
+
+//Make request
+request.open('GET','',true);
+request.send(null);
+};
+
