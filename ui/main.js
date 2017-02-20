1574ab7e37a8a9a0cada32250451ce81ffb1ecf3
sumankumarbhadra <sumankumarbhadra@gmail.com>
2017-02-20 12:14:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,27 +1,22 @@
 console.log('Loaded!');
  
-var button = document.getElementById('c');
-
-button.onclick = function() {
+ window.onload= function()
+    {
+      var request= new XMLHttpRequest();
     
-    // create a request object
-    var request = new XMLHttpRequest();
+      request.onreadystatechange=function()
+      {
+        if(request.readyState==4 && request.status==200 )
+        { 
+             var counter =request.responseText;
+             var span=document.getElementById("display");
+             span.innerHTML=" visitor count: "+likecounter;
+            
+        }
+        
+     };
     
-    // capture the response and store it in a variable
-    request.onreadystatechange = function() {
-      if (request.readyState === XMLHttpRequest.DONE) {
-          // take some action
-          if (request.status === 200) {
-              var likecounter = request.responseText;
-              var span = document.getElementById('display');
-              span.innerHTML = likecounter.toString();
-          }
-      }  
-      // not done yet
-    };
-    
-    // make the request
-    request.open('GET', '/likes', true);
+    request.open('GET',"/likes",true);
     request.send(null);
-};
+   };
 
