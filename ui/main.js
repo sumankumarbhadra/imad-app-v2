de616e1e19ad8eff9a8d9f21235e24cc83140e36
sumankumarbhadra <sumankumarbhadra@gmail.com>
2017-02-20 13:48:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,3 +21,26 @@ console.log('Loaded!');
    };
 
 //code for like count
+
+
+console.log('Loaded!');
+ 
+ window.onload= function()
+    {
+      var request= new XMLHttpRequest();
+    
+      request.onreadystatechange=function()
+      {
+        if(request.readyState == 4 && request.status==200 )
+        { 
+             var counter =request.responseText;
+             var span=document.getElementById("display");
+             span.innerHTML=counter;
+            
+        }
+        
+     };
+    
+    request.open('GET',"/likes",true);
+    request.send(null);
+   };
