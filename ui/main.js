04ec0334b38fc41dc1c2b53cafc3dfd91710e5c0
sumankumarbhadra <sumankumarbhadra@gmail.com>
2017-04-02 11:19:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,36 +43,6 @@ window.onload = function()
     request.open('GET',"/likes",true);
     request.send(null);
    };
-   
-    if (window.iframe.src.match('content.html') !== null)
-    {
-    var submit=document.getElementById("submit");
-    submit.onclick= function()
-    {
-      var request= new XMLHttpRequest();
-    
-      request.onreadystatechange=function()
-      {
-        if(request.readyState == 4 && request.status==200 )
-        { 
-             var comment =request.responseText;
-             comment= JSON.parse(comment);
-             var list = '';
-             for(var i = 0;i<comment.length;i++)
-             {
-                 list+='<li>'+comment[i]+'</li>';
-             }
-             var ul=document.getElementById("comments");
-             ul.innerHTML =list;
-            
-        }
-        
-     };
-    var commentInput = document.getElementById('comment');
-    var comment = commentInput.value;
-    request.open('GET',"/submit-comment?comment="+comment,true);
-    request.send(null);
-   };
-    }
+  
    };
 
